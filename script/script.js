7b490e8a68db8df9d3edf7bd7f06f13f687bab8d
Eduardo do Vale <edu.dovale@gmail.com>
2020-05-17 22:04:17 -0300

    Update do dia
    Quem somos / Index / Correções

---
+++ script/script.js
@@ -0,0 +1,33 @@
+let mensagem1 = document.querySelector(".msgJoao");
+
+// mostra a mensagem
+function showMessage1(){   
+   mensagem1.style.display = "block";
+ }
+// esconde a mensagem
+function hideMessage1(){
+  mensagem1.style.display = "none"; 
+}
+
+let mensagem2 = document.querySelector(".msgEdu") ;
+
+// mostra a mensagem
+function showMessage2(){   
+   mensagem2.style.display = "block";   
+ }
+// esconde a mensagem
+function hideMessage2(){
+  mensagem2.style.display = "none"; 
+}
+
+let mensagem3 = document.querySelector(".msgVlad") ;
+
+// mostra a mensagem
+function showMessage3(){   
+   mensagem3.style.display = "block";   
+ }
+// esconde a mensagem
+function hideMessage3(){
+  mensagem3.style.display = "none"; 
+}
+
